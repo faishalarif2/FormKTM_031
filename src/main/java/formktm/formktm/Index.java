e59f4131c5ab49af387ef40a3b75d0209fbd011e
faishalarif2 <faishalarifn02@gmail.com>
2022-11-14 21:42:47 +0700

    index java

---
+++ src/main/java/formktm/formktm/Index.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package formktm.formktm;
+
+/**
+ *
+ * @author NITRO
+ */
+public class Index {
+    private String name;
+    private String nim;
+    private String jurusan;
+    private String gambar;
+    public Index(){
+        super();
+    }
+    public String getname(){
+        return name;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public String getnim(){
+        return nim;
+    }
+    public void setnim(String nim){
+        this.nim = nim;
+    }
+    public String getjurusan(){
+        return jurusan;
+    }
+    public void setjurusan(String nim){
+        this.jurusan = jurusan;
+    }
+    public String getgambar(){
+        return gambar;
+    }
+    public void setgambar(String gambar){
+        this.gambar = gambar;
+    }
+}
