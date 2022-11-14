b395eb34216b17ed6718637444fe7dd008128eda
faishalarif2 <faishalarifn02@gmail.com>
2022-11-14 14:52:23 +0700

    commit

---
+++ src/main/java/formktm/formktm/FormktmApplication.java
@@ -0,0 +1,13 @@
+package formktm.formktm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormktmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormktmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/formktm/formktm/FormktmApplicationTests.java
@@ -0,0 +1,13 @@
+package formktm.formktm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormktmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
